6dc2f224f531d29cd3dab0c91910bb7488499f6e
hoangndh2 <your.email@example.com~cle>
2024-04-21 16:43:27 +0700

    Fix comment

--- starter/src/ShelfChanger.js
+++ starter/src/ShelfChanger.js
@@ -1,13 +1,23 @@
 import React from "react";
 import * as BookAPI from "./BooksAPI.js";
+import { useLocation } from 'react-router-dom';
+
 const ShelfChanger = ({ book,setSearchResult }) => {
+  const location = useLocation();
+  const activeLink = location.pathname;
   const handleUpdate = (e) => {
     BookAPI.update(book,e.target.value).then(
+      renderListSearch()
+    )
+  };
+
+  const renderListSearch = () => {
+    if(activeLink === '/search'){
       BookAPI.getAll().then((res) => {
         setSearchResult(res);
       })
-    )
-  };
+    }
+  }
 
   return (
     <div className="book-shelf-changer">
